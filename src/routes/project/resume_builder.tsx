979f3bf6455c97ec30a81767e983663cd9eb033e
Daniel Sandstrom <github@danielsandstrom.org>
2025-11-16 15:52:11 +0100

    Editing Resume Builder Project text

--- src/routes/project/resume_builder.tsx
+++ src/routes/project/resume_builder.tsx
@@ -9,20 +9,21 @@ function RouteComponent() {
   return <div className='project-page-container'>
     <Project
       name="Resume Builder App"
-      description="A new solution for SALT consultants to build, version, and manage CVs. Separates resume data from theme, allowing flexible theme changes and improved PDF quality."
+      description="A new solution for SALT consultants to build, version, and manage CVs. Separates resume data from theme for more flexible theme management, and improved the PDF quality. Other features: LinkedIn import, translation, custom theme language built on html."
       highlights={[
         'Product Lead — defined product vision and scope',
         'Presentation Lead — responsible for presentation structure and delegation',
-        'Contributed across all code areas as developer',
-        'Implemented LinkedIn conversion, translation, theme management, and PDF export',
+        'Contributed across the whole codebase',
       ]}
       tech={[
-        'TanStack React',
-        'Spring Boot (AI microservice)',
+        'React',
+        'TanStack Router & Query',
+        'Spring Boot',
+        'Docker',
         'Fly.io',
         'Gemini API (AI)',
       ]}
-      reflections={`This project deepened my understanding of AI in development. It taught me how to balance AI assistance with manual coding, and how good prompting and rule documents increase development speed and code quality.`}
+      reflections={`It was good experience as a Product Lead, to work together with the team to form a vision on what is or is not vital to the product. This project also deepened my understanding of AI in development. It taught me how to balance AI assistance with manual coding, and how good prompting and rule documents increase development speed and code quality for AI.`}
       links={{
         github: 'https://github.com/salt-community/json2resume/',
         live: 'https://frontend-proud-butterfly-9919.fly.dev/',
